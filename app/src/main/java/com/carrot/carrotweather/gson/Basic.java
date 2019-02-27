53a360bbe7f14e73a8d762b73360a37dab0726dc
David <carrot586@163.com>
2019-02-27 23:14:17 +0800

    简单实现了主要功能。

---
+++ app/src/main/java/com/carrot/carrotweather/gson/Basic.java
@@ -0,0 +1,22 @@
+package com.carrot.carrotweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Basic {
+
+    @SerializedName("location")
+    public String cityName;
+
+    @SerializedName("cid")
+    public String weatherId;
+
+    public Update update;
+
+    public class Update{
+
+        @SerializedName("loc")
+        public String updateTime;
+
+    }
+
+}
